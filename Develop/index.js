9b2d614196cc41a01badac938f46c23c42231569
Mark Napolitano <156marknap@gmail.omgit config --global user.name oMarkNx>
2023-01-18 18:31:09 +0800

    Added inquirer package and began creating questions array, worked on some code to take template literals and create a new README file with the information gathered from the questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,58 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your Name?",
+    name: "name",
+  },  
+  {
+    type: "input",
+    message: "What is your Project Title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Enter the description for your README:",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Enter the installation instructions for your README:",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Enter the usage information for your README:",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Enter the contribution guidelines for your README:",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "Enter the test instructions for your README:",
+    name: "test",
+  },
+  
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile() {
+  inquirer.prompt(questions)
+  .then((response) =>
+  fs.appendFile(`${(response.name.toUpperCase())}'s-README.md`, `# ${(response.title)}\n## Description\n${(response.description)}\n`, (err) =>
+  err ? console.error(err) : console.log('README Created!')))
+}
+    
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  writeToFile()
+}
 
 // Function call to initialize app
 init();
